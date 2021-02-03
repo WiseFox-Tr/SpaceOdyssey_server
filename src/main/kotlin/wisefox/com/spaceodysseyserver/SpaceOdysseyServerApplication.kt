e58fa46d7f10cdbe044cac344ce14c225763e3a7
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-03 17:41:09 +0100

    initial commit with base project

---
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/SpaceOdysseyServerApplication.kt
@@ -0,0 +1,11 @@
+package wisefox.com.spaceodysseyserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpaceOdysseyServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpaceOdysseyServerApplication>(*args)
+}
---
+++ src/test/kotlin/wisefox/com/spaceodysseyserver/SpaceOdysseyServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package wisefox.com.spaceodysseyserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpaceOdysseyServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
