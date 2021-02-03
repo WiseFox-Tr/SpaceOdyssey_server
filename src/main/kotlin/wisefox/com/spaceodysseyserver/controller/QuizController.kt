680ae0b94f1a93905ac756b9fd3d0c201065345c
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-03 18:20:14 +0100

    add @RestController to QUizController class

--- src/main/kotlin/wisefox/com/spaceodysseyserver/controller/QuizController.kt
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/controller/QuizController.kt
@@ -1,7 +1,9 @@
 package wisefox.com.spaceodysseyserver.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 class QuizController {
 
     //http://localhost:8080/testServer
