d5625b9c21edbcac201800d73aa1c9329ee0813a
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-05 10:40:29 +0100

    change json received by getQuestions & little improvements

--- src/main/kotlin/wisefox/com/spaceodysseyserver/controller/QuizController.kt
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/controller/QuizController.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import wisefox.com.spaceodysseyserver.model.*
+import wisefox.com.spaceodysseyserver.utils.traceServerRequest
 
 
 @RestController
@@ -20,29 +21,24 @@ class QuizController {
         println("/testServer")
         return "Server is ok"
     }
-
-
-
-
+    
     /* Json recieve for getQuestion *************
     {
-        "level" :
-        {
-            "lvl_id" : 1,
-            "lvl_name": "Débutant"
-        },
-        "theme" :
-        {
-            "theme_id" : 1,
-            "theme_name": "Systèmes solaires"
-        }
+      "levelBean": {
+        "lvl_id": 1,
+        "lvl_name": "Débutant"
+      },
+      "themeBean": {
+        "theme_id": 1,
+        "theme_name": "Systèmes planétaires"
+      }
     }
     ******************************************* */
     //http://localhost:8080/getQuestions
     @PostMapping("getQuestions")
-    fun getQuestions(@RequestBody quizParamBean: QuizParamBean): ResponseCodeBean<List<QuestionBean>> {
-        print(("/getQuestions"))
-        print("Quiz param : $quizParamBean")
+    fun getQuestions(@RequestBody paramsBean: ParamsBean): ResponseCodeBean<List<QuestionBean>> {
+        traceServerRequest("/getQuestions")
+        print("Quiz param : $paramsBean")
 
         //todo: once the database will be up and running -> manage errors with a try catch bloc
 
@@ -64,9 +60,8 @@ class QuizController {
             "Mars",
             "Explication simple",
             LevelBean(1, "Débutant"),
-            ThemeBean(1, "Systèmes solaires")
+            ThemeBean(1, "Systèmes planétaires")
         ))
         return questionsToSend
     }
-
 }
