0386c21dcd0167a94eaac557497a6f9cad7fadd8
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-08 18:07:11 +0100

    create quiz dao interface that inherit from JpaRepository and add a question query by theme & level

---
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/model/dao/QuizDao.kt
@@ -0,0 +1,14 @@
+package wisefox.com.spaceodysseyserver.model
+
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
+
+
+@Repository
+interface QuizDAO : JpaRepository<QuestionBean, Int> {
+
+    @Query(value = "SELECT * FROM question WHERE lvl_id = ?1 AND theme_id = ?2", nativeQuery = true)
+    fun findQuestionsByLevelIdAndThemeId(lvl_id : Byte, theme_id : Byte) : ArrayList<QuestionBean>
+}
