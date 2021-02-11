bbee2da349edef733c699cd7c5976f1be7e1143f
Maxime Traverse <maxime.traverse@orange.fr>
2021-02-11 10:22:49 +0100

    const file

---
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/model/ServerConst.kt
@@ -0,0 +1,15 @@
+package wisefox.com.spaceodysseyserver.model
+
+
+class ServerConst {
+
+    companion object {
+        //error
+        const val ERR_LACK_OF_QUEST = 800
+
+        //nbQuestionsByGame
+        const val NB_QUESTIONS = 10
+    }
+}
+
+
