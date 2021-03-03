f06e97ff2344a54b18a55bfa04de9f7f475a28ce
Maxime Traverse <maxime.traverse@orange.fr>
2021-03-03 15:45:14 +0100

    add an error code for params request

--- src/main/kotlin/wisefox/com/spaceodysseyserver/model/ServerConst.kt
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/model/ServerConst.kt
@@ -6,6 +6,7 @@ class ServerConst {
     companion object {
         //error
         const val ERR_QUEST = 800
+        const val ERR_PARAMS = 801
 
         //nbQuestionsByGame
         const val NB_QUESTIONS = 10
