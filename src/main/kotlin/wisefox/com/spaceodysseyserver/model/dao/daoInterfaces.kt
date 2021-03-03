c6ce0dca19c61b99ae5f6f89d024af69758a6aa5
Maxime Traverse <maxime.traverse@orange.fr>
2021-03-03 15:46:16 +0100

    rename QuizDAO file in daoInterfaces + add interfaces for theme & level requests

--- src/main/kotlin/wisefox/com/spaceodysseyserver/model/dao/daoInterfaces.kt
+++ src/main/kotlin/wisefox/com/spaceodysseyserver/model/dao/daoInterfaces.kt
@@ -1,14 +1,22 @@
-package wisefox.com.spaceodysseyserver.model
+package wisefox.com.spaceodysseyserver.model.dao
 
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import wisefox.com.spaceodysseyserver.model.Level
+import wisefox.com.spaceodysseyserver.model.Question
+import wisefox.com.spaceodysseyserver.model.Theme
 
 
 @Repository
 interface QuizDAO : JpaRepository<Question, Int> {
-
     @Query(value = "SELECT * FROM question WHERE lvl_id = ?1 AND theme_id = ?2", nativeQuery = true)
     fun findQuestionsByLevelIdAndThemeId(lvl_id : Byte, theme_id : Byte) : ArrayList<Question>
 }
+
+@Repository
+interface LevelDAO : JpaRepository<Level, Byte>
+
+@Repository
+interface ThemeDAO : JpaRepository<Theme, Byte>
